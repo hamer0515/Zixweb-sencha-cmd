82c0aa400922c159036e3af912c2c92452adc616
hongbo.gan <hongbo.gan@yeepay.com>
2013-10-31 13:33:27 +0800

    富汇易达中涉及到4个控件

---
+++ app/view/component/FhydAcct.js
@@ -0,0 +1,58 @@
+Ext.define('Zixweb.view.component.FhydAcct', {
+			extend : 'Ext.form.ComboBox',
+			alias : 'widget.fhydacct',
+			width : 516,
+			queryMode : 'local',
+			anyMatch : true,
+			listeners : {
+				blur : function(self, The, eOpts) {
+					var value = self.getValue();
+					var result = self.getStore().queryBy(function(record) {
+								if (record.data.id == value) {
+									return true;
+								}
+								return false;
+							});
+					if (result.length == 0) {
+						self.setValue('');
+					}
+				}
+			},
+			initComponent : function() {
+				this.store = new Ext.data.Store({
+							fields : ['id', 'name'],
+							autoLoad : true,
+
+							proxy : {
+								type : 'ajax',
+								url : 'base/fhydacct'
+							},
+							listeners : {
+								load : function(thiz, records, successful,
+										eOpts) {
+									if (!successful) {
+										Ext.MessageBox.show({
+													title : '警告',
+													msg : '富汇易达帐号字典数据加载失败,请联系管理员',
+													buttons : Ext.Msg.YES,
+													icon : Ext.Msg.ERROR
+												});
+										return;
+									}
+									var jsonData = thiz.proxy.reader.jsonData.success;
+									if (jsonData && jsonData === 'forbidden') {
+										Ext.MessageBox.show({
+													title : '警告',
+													msg : '抱歉，富汇易达帐号字典数据访问权限',
+													buttons : Ext.Msg.YES,
+													icon : Ext.Msg.ERROR
+												});
+									}
+								}
+							}
+						});
+				this.valueField = 'id';
+				this.displayField = 'name';
+				this.callParent(arguments);
+			}
+		});
