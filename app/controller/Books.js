614eb4154aa7702cf49e26d9f2b4beddf66056be
hongbo.gan <hongbo.gan@yeepay.com>
2013-12-30 09:21:09 +0800

    bug for 2.6.5

--- app/controller/Books.js
+++ app/controller/Books.js
@@ -19,9 +19,9 @@ Ext.define('Zixweb.controller.Books', {
 					'book.hist.txamt_dqr_oys', 'book.hist.wlzj_ysbf',
 					'book.hist.wlzj_yszy', 'book.hist.wlzj_yfbf',
 					'book.hist.wlzj_yfzy', 'book.hist.adjust_qc',
-					'book.hist.income_add', 'fhydbook.hist.deposit_fhyd',
-					'fhydbook.hist.camt_fhyd', 'fhydbook.hist.ypsc_fhyd',
-					'fhydbook.hist.camt_dgd_fhyd',
+					'book.hist.income_add', 'book.hist.bsc_jf',
+					'fhydbook.hist.deposit_fhyd', 'fhydbook.hist.camt_fhyd',
+					'fhydbook.hist.ypsc_fhyd', 'fhydbook.hist.camt_dgd_fhyd',
 					'fhydbook.hist.yp_acct_fhyd',
 					'fhydbook.hist.yufamt_ch_fhyd',
 					'fhydbook.hist.nctxamt_dqr_oys_fhyd',
@@ -76,5 +76,5 @@ Ext.define('Zixweb.controller.Books', {
 					'book.detail.deposit_zyzj', 'book.detail.txamt_dgd',
 					'book.detail.txamt_yhys', 'book.detail.bamt_yhys',
 					'book.detail.blc', 'book.detail.adjust_qc',
-					'book.detail.income_add']
+					'book.detail.income_add', 'book.detail.bsc_jf']
 		});
